eeec7555974199c865954dcdd455a519203f64eb
yomna raouf <yomnaraouf99@gmail.com>
2021-04-23 02:28:08 +0200

    Refactoring formHandler

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,43 +1,46 @@
 const { urlChecker } = require('./urlChecker');
 
-function handleSubmit(event) {
+// handling URLL Form submition
+const handleSubmit = (event) =>  {
     event.preventDefault();
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value;
 
-    if (urlChecker(formText)) {
+    // Validating user input
+    let userUrl = document.getElementById('name').value;
 
-    postData(formText)
-    .then(function(res) {
-        console.log('client side response', res);
-        document.getElementById('irony').innerHTML = `<span> Irony: </span> ${res.irony}`;
-        document.getElementById('confidence').innerHTML = `<span> Confidence: </span> ${res.confidence}`;
-        document.getElementById('subjectivity').innerHTML = `<span> Subjectivity: </span>  ${res.subjectivity}`;
-        document.getElementById('polarity').innerHTML = `<span> Polarity: </span> ${res.score_tag}`;
-    })
-  } else {
-    alert = "Please enter valid URL";
+    if (urlChecker(userUrl)) {
+        postData(userUrl)
+            .then((res) =>  {
+                let html = `
+                    <div id="irony"> <span> Irony: </span> ${res.irony} </div>
+                    <div id="confidence"> <span> Confidence: </span> ${res.confidence} </div>
+                    <div id="subjectivity"> <span> Subjectivity: </span>  ${res.subjectivity} </div>
+                    <div id="polarity"> <span> Polarity: </span> ${res.score_tag} </div>
+                `
+                document.getElementById('results').innerHTML = html ;
+            })
+    } else {
+        alert("Please enter valid URL");
+    }
 }
-}
 
-const postData = async(url ="") => {
-    const response = await fetch('http://localhost:8083/test', {
+// POSTING User Input to server
+const postData = async (url = "") => {
+    try {
+        const response = await fetch('http://localhost:8083/NLP', {
             method: 'POST',
             mode: 'cors',
             credentials: 'same-origin',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify( {"url": url }),
+            body: JSON.stringify( { url }),
         });
 
-        try {
-            const newData = await response.json();
-            console.log(newData);
-            return newData;
-        } catch (error) {
-            console.log("error", error);
-        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 export { handleSubmit }
