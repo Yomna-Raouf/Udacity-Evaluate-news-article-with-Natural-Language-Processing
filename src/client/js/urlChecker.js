7a56eca123e0dbcc2873f1bdfb8774e2307446c1
yomna raouf <yomnaraouf99@gmail.com>
2021-04-23 02:31:25 +0200

    refactoring

--- src/client/js/urlChecker.js
+++ src/client/js/urlChecker.js
@@ -1,12 +1,12 @@
-function urlChecker(submitedUrl){
+const urlChecker = (submitedUrl) => {
+
+    // Regular Expression to validate url
     let RegEx = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
     const url = submitedUrl;
     
-    if (RegEx.test(url)) {
-        return true;
-    } else {
-        return false;
-    }
+    if (RegEx.test(url)) return true;
+    return false;
+
 }
 
 export { urlChecker } 
